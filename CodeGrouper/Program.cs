eaf560c07787204f4080194cc2dc05fce5264580
Marx Bitware <info@marxbitware.co.uk>
2021-02-27 17:52:43 +0700

    Add project files.

---
+++ CodeGrouper/Program.cs
@@ -0,0 +1,21 @@
+﻿using LibCodeGroup;
+using System;
+using System.Security.Cryptography;
+
+namespace CodeGrouper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] file = new byte[32768];
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(file);
+
+            string codedFile = CodeGroup.ConvertToCode(file);
+            byte[] decodefile = CodeGroup.ConvertFromCode(codedFile);
+
+            Console.ReadLine();
+        }
+    }
+}
