7774e099e39aa5fd2d418316c224115645c4b63e
Marx Bitware <info@marxbitware.co.uk>
2021-03-02 18:39:56 +0700

    Added CodeTalker, a TTS output for the encoder/decoder

--- CodeGrouper/Program.cs
+++ CodeGrouper/Program.cs
@@ -15,11 +15,15 @@ namespace CodeGrouper
             string[] lookup = CodeGroup.GenerateLookup();  //Generate a random lookup
 
             string codedFile = CodeGroup.ConvertToCode(file);
+
+            //CodeTalker.Speak(codedFile);  //This may only work on Windows - TTS output
+
             byte[] decodefile = CodeGroup.ConvertFromCode(codedFile);
 
             string[] lookup5 = CodeGroup5.GenerateLookup();  //Generate a random lookup
 
             string coded5File = CodeGroup5.ConvertToCode(file);
+            //CodeTalker.Speak(coded5File);  //This may only work on Windows - TTS output
             byte[] decode5file = CodeGroup5.ConvertFromCode(coded5File);
 
             Console.ReadLine();
---
+++ LibCodeGroup/CodeTalker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Speech.Synthesis;
+using System.Text;
+
+namespace LibCodeGroup
+{
+    public static class CodeTalker
+    {
+        private static string AddWhiteSpace(string code)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < code.Length; i++)
+            {
+                sb.Append(code[i] + " ");
+            }
+
+            return sb.ToString();
+        }
+
+        private static string Split5(string code)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (i % 10 == 0  && i != 0)
+                    sb.Append(code[i] + ", ");
+                else
+                    sb.Append(code[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static void Speak(string code)
+        {
+            code = AddWhiteSpace(code);
+            code = Split5(code);
+
+            SpeechSynthesizer speech = new SpeechSynthesizer();
+            speech.Rate = -4;
+            speech.SpeakAsync(code);
+
+            Console.ReadLine();
+        }
+    }
+}
